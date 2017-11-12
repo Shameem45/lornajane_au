6e098b59ae1899b0d594d3b2123495391adc7206
unknown <shameem.ahamed@borngroup.com>
2017-11-12 20:28:44 +0530

    feedbackChanges

--- js/custom.js
+++ js/custom.js
@@ -11,7 +11,10 @@
 		dots:true,
 		fade:true,
 		speed: 2000,
-		waitForAnimate: false
+		waitForAnimate: false,
+		pauseOnHover:false,
+		autoplay: true,
+		autoplaySpeed: 12000
 	});
 	$(".lj-hotspot-icon").click(function(event){
 		event.stopPropagation();
@@ -22,10 +25,13 @@
 		$('.lj-category-article').hide();
 	});
 	
-	$('body').on('click', '.lj-category-item', function() {
+	$('body').on('click', '.lj-category-item, .lg-hs-close', function() {
 		$(".lj-hs-content").hide();
 		$(".lj-hotspot-icon").show();
 		$('.lj-category-article').show();
+	});
+	$("body").on("click", ".lg-hs-image", function(event){
+		event.stopPropagation();
 	});
 	$(".lj-side-menu li a").click(function(event){
 		$('.lj-category-article').show();
@@ -33,6 +39,7 @@
 		$(this).addClass("active");
 		var slideIndex = $(this).data("index");
 		$('.lj-gift-sliders').slick('slickGoTo',slideIndex);
+		$('.lj-category-slider').slick('slickGoTo',0)
 	});
 	
 
