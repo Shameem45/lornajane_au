8fbcd3a23142f7fc2bf93247ee8257e0ca2a3315
Shameem Ahamed.N Nagoor Meeran <shameem.a@wearefmg.net>
2017-11-16 05:22:47 +0530

    feebackClient

--- js/custom.js
+++ js/custom.js
@@ -10,22 +10,35 @@
 		infinite: false,
 		dots:true,
 		fade:true,
-		speed: 2000,
+		speed: 800,
 		waitForAnimate: false,
 		pauseOnHover:false,
 		autoplay: false,
 		//autoplaySpeed: 12000
 	});
-	$(".lj-hotspot-icon").click(function(event){
+	$(".lj-hotspot-icon").mouseenter(function(event){
 		event.stopPropagation();
-		$(".lj-hotspot-icon").show();
-		$(this).hide();
-		$(".lj-hs-content").hide();
-		$(this).next('.lj-hs-content').show();
+		$(".lj-hotspot-icon").removeClass('active');
+		$(this).hide();	
+		$('.lg-hs-close').hide();
+
+		if($(this).next('.lj-hs-content:visible').length == 0){
+			$(".lj-hs-content").hide();
+			$(this).next('.lj-hs-content').show();
+		}
+
+		$(this).next('.lj-hs-content').find('.lg-hs-close').fadeIn(1000);
+		$(this).addClass('active').fadeIn('fast');
 		$('.lj-category-article').hide();
+		
+		if($(this).parents('.lj-category-item').find('.lj-popup-overlay').length == 0){
+			$('<div class="lj-popup-overlay"></div>').insertBefore('.lj-hotspot.first');
+			$('.lj-popup-overlay').fadeIn('fast');
+		}
 	});
 	
 	$('body').on('click', '.lj-category-item, .lg-hs-close', function() {
+		$('.lj-popup-overlay').remove();
 		$(".lj-hs-content").hide();
 		$(".lj-hotspot-icon").show();
 		$('.lj-category-article').show();
@@ -41,12 +54,14 @@
 		$(this).addClass("active");
 		var slideIndex = $(this).data("index");
 		$('.lj-gift-sliders').slick('slickGoTo',slideIndex);
-		$('.lj-category-slider').slick('slickGoTo',0)
+		$('.lj-category-slider').slick('slickGoTo',0);
+		$('.lj-popup-overlay').remove();
 	});
 	$(".slick-dots li").click(function(event){
 		$('.lj-category-article').show();
 		$(".lj-hs-content").hide();
 		$(".lj-hotspot-icon").show();
+		$('.lj-popup-overlay').remove();
 	});
 	
 
